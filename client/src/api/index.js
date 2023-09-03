b01bdae5ee625e93c3cc46245fc0d547ea0e9204
sreyasilp <sreyas.tp@experionglobal.com>
2023-09-03 14:57:13 +0530

    change base url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: " http://localhost:4444" });
+const API = axios.create({ baseURL: "https://zeevoc-server.onrender.com" });
 
 export const signUp = (formData) => API.post("/user/signup/userType", formData);
 export const signIn = (formData) => API.post("/user/signin", formData);
