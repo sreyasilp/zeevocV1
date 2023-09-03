a172d409e530eff5e23e83a78565725442069675
sreyasilp <sreyas.tp@experionglobal.com>
2023-09-03 14:44:31 +0530

    add dark theme to many pages

--- client/src/elements/Blog.jsx
+++ client/src/elements/Blog.jsx
@@ -3,50 +3,54 @@ import PageHelmet from "../component/common/Helmet";
 import Breadcrumb from "../elements/common/Breadcrumb";
 import Pagination from "../elements/common/Pagination";
 import BlogList from "../elements/blog/BlogList";
-import ScrollToTop from 'react-scroll-up';
+import ScrollToTop from "react-scroll-up";
 import { FiChevronUp } from "react-icons/fi";
 import Header from "../component/header/Header";
 import Footer from "../component/footer/Footer";
 
-class Blog extends Component{
-    render(){
-        return(
-            <React.Fragment>
-                <PageHelmet pageTitle='Blog' />
+class Blog extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="active-dark">
+          <PageHelmet pageTitle="Blog" />
 
-                <Header headertransparent="header--transparent" colorblack="color--black" logoname="logo.png" />
-                {/* Start Breadcrump Area */}
-                <Breadcrumb title={'Blog List'}   />
-                {/* End Breadcrump Area */}
+          <Header
+            headertransparent="header--transparent"
+            colorblack="color--black"
+            logoname="logo.png"
+          />
+          {/* Start Breadcrump Area */}
+          <Breadcrumb title={"Blog List"} />
+          {/* End Breadcrump Area */}
 
-
-                {/* Start Blog Area */}
-                <div className="rn-blog-area ptb--120 bg_color--1">
-                    <div className="container">
-                        <BlogList />
-                        <div className="row mt--20">
-                            <div className="col-lg-12">
-                                {/* Start Pagination Area */}
-                                <Pagination />
-                                {/* End Pagination Area */}
-                            </div>
-                        </div>
-                    </div>
+          {/* Start Blog Area */}
+          <div className="rn-blog-area ptb--120 bg_color--1">
+            <div className="container">
+              <BlogList />
+              <div className="row mt--20">
+                <div className="col-lg-12">
+                  {/* Start Pagination Area */}
+                  <Pagination />
+                  {/* End Pagination Area */}
                 </div>
-                {/* End Blog Area */}
-                
-                {/* Start Back To Top */}
-                <div className="backto-top">
-                    <ScrollToTop showUnder={160}>
-                        <FiChevronUp />
-                    </ScrollToTop>
-                </div>
-                {/* End Back To Top */}
-                
-                <Footer /> 
+              </div>
+            </div>
+          </div>
+          {/* End Blog Area */}
 
-            </React.Fragment>
-        )
-    }
+          {/* Start Back To Top */}
+          <div className="backto-top">
+            <ScrollToTop showUnder={160}>
+              <FiChevronUp />
+            </ScrollToTop>
+          </div>
+          {/* End Back To Top */}
+
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 export default Blog;
--- client/src/elements/SignUp.jsx
+++ client/src/elements/SignUp.jsx
@@ -1,52 +1,57 @@
-import React from 'react'
+import React from "react";
 import PageHelmet from "../component/common/Helmet";
-import BreadcrumbLogin from './common/LoginBreadcrumb';
-import ScrollToTop from 'react-scroll-up';
+import BreadcrumbLogin from "./common/LoginBreadcrumb";
+import ScrollToTop from "react-scroll-up";
 import { FiChevronUp } from "react-icons/fi";
 import Header from "../component/header/Header";
 import Footer from "../component/footer/Footer";
-import SignUpForm from './signup/SignUpForm';
+import SignUpForm from "./signup/SignUpForm";
 
 const SignUp = () => {
-    return (
-        <>
-            <PageHelmet pageTitle='Contact Form' />
+  return (
+    <>
+      <div className="active-dark">
+        <PageHelmet pageTitle="Contact Form" />
 
-            {/* Start Header Area  */}
-            <Header headertransparent="header--transparent" colorblack="color--black" logoname="logo.png" />
-            {/* End Header Area  */}
-            
-            {/* Start Breadcrump Area */}
-            <BreadcrumbLogin title={'Contact Form'}   />
-            {/* End Breadcrump Area */}
+        {/* Start Header Area  */}
+        <Header
+          headertransparent="header--transparent"
+          colorblack="color--black"
+          logoname="logo.png"
+        />
+        {/* End Header Area  */}
 
-            {/* Start Page Wrapper  */}
-            <main className="page-wrapper">
+        {/* Start Breadcrump Area */}
+        <BreadcrumbLogin title={"Contact Form"} />
+        {/* End Breadcrump Area */}
 
-                {/* Start Contact Form  */}
-                <div className="rn-contact-form-area ptb--120 bg_color--1">
-                    <SignUpForm contactTitle="Login" contactImages="/assets/images/about/about-6.jpg" />
-                </div>
-                {/* Start Contact Form  */}
+        {/* Start Page Wrapper  */}
+        <main className="page-wrapper">
+          {/* Start Contact Form  */}
+          <div className="rn-contact-form-area ptb--120 bg_color--1">
+            <SignUpForm
+              contactTitle="Login"
+              contactImages="/assets/images/about/about-6.jpg"
+            />
+          </div>
+          {/* Start Contact Form  */}
+        </main>
+        {/* End Page Wrapper  */}
 
-            </main>
-            {/* End Page Wrapper  */}
+        {/* Start Back To Top */}
+        <div className="backto-top">
+          <ScrollToTop showUnder={160}>
+            <FiChevronUp />
+          </ScrollToTop>
+        </div>
+        {/* End Back To Top */}
 
-            {/* Start Back To Top */}
-            <div className="backto-top">
-                <ScrollToTop showUnder={160}>
-                    <FiChevronUp />
-                </ScrollToTop>
-            </div>
-            {/* End Back To Top */}
-            
-            {/* Start Footer Area  */}
-            <Footer />
-            {/* End Footer Area  */}
-
-        </>
-        
-    )
-}
+        {/* Start Footer Area  */}
+        <Footer />
+        {/* End Footer Area  */}
+      </div>
+    </>
+  );
+};
 
 export default SignUp;
