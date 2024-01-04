e384cc18af301d7213ad73e117ee5a598097d726
sreyasilp <sreyas.tp@experionglobal.com>
2024-01-04 13:35:51 +0530

    add

---
+++ client/build/precache-manifest.b8b0c9145557d6fa55090e836de27a62.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abcb5db47a97a6e32d59f07ba9fdaa94",
+    "url": "https//:zeevoc.com/index.html"
+  },
+  {
+    "revision": "869d394d24f0996874e6",
+    "url": "https//:zeevoc.com/static/css/2.32d5bab5.chunk.css"
+  },
+  {
+    "revision": "2352da1470d657bc4c99",
+    "url": "https//:zeevoc.com/static/css/main.5115d8fb.chunk.css"
+  },
+  {
+    "revision": "869d394d24f0996874e6",
+    "url": "https//:zeevoc.com/static/js/2.a2c80220.chunk.js"
+  },
+  {
+    "revision": "28d2157e628d5790821d",
+    "url": "https//:zeevoc.com/static/js/3.dca874c7.chunk.js"
+  },
+  {
+    "revision": "2352da1470d657bc4c99",
+    "url": "https//:zeevoc.com/static/js/main.6ce384e8.chunk.js"
+  },
+  {
+    "revision": "6a5dc89ac519960bf4cc",
+    "url": "https//:zeevoc.com/static/js/runtime~main.7630a4e8.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "https//:zeevoc.com/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "https//:zeevoc.com/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "https//:zeevoc.com/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "https//:zeevoc.com/static/media/slick.f97e3bbf.svg"
+  }
+]);
