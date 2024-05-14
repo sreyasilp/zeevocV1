8537b10e7b3e16d35f7239a762ecb179197193cb
sreyasilp <er.sreyas.tp@gmail.com>
2024-05-14 12:02:52 +0530

    added payment and service details

--- client/src/component/header/Header.jsx
+++ client/src/component/header/Header.jsx
@@ -53,7 +53,7 @@ const Header = (props) => {
         logoUrl = <img src="/assets/images/logo/logo.png" alt="Zeevoc" />;
     }
 
-    return(
+    return (
         <header className={`header-area formobile-menu header--transparent ${color}`}>
             <div className="header-wrapper" id="header-wrapper">
                 <div className="header-left">
@@ -66,83 +66,17 @@ const Header = (props) => {
                 <div className="header-right">
                     <nav className="mainmenunav d-lg-block">
                         <ul className="mainmenu">
-                            {/* <li className="has-droupdown"><Link to="/">Home</Link>
-                                <ul className="submenu">
-                                    <li><Link to="/main-demo">Main Demo</Link></li>
-                                    <li><Link to="/dark-main-demo">Main Demo Dark</Link></li>
-                                    <li><Link to="/creative-agency">Creative Agency</Link></li>
-                                    <li><Link to="/creative-landing">Creative One Page</Link></li>
-                                    <li><Link to="/creative-portfolio">Creative Portfolio</Link></li>
-                                    <li><Link to="/personal-portfolio">Personal Portfolio</Link></li>
-                                    <li><Link to="/portfolio-landing">Portfolio One Page</Link></li>
-                                    <li><Link to="/dark-portfolio-landing">Portfolio One Page 02</Link></li>
-                                    <li><Link to="/digital-agency">Digital Agency</Link></li>
-                                    <li><Link to="/startup">Startup</Link></li>
-                                    <li><Link to="/paralax">Paralax</Link></li>
-                                    <li><Link to="/portfolio-home">Minimal Portfolio</Link></li>
-                                    <li><Link to="/business">Business</Link></li>
-                                    <li><Link to="/home-particles">Home Particles</Link></li>
-                                    <li><Link to="/studio-agency">Studio Agency</Link></li>
-                                    <li><Link to="/designer-portfolio">Designer Portfolio</Link></li>
-                                    <li><Link to="/interactive-agency">Interactive Agency</Link></li>
-                                </ul>
-                            </li> */}
                             <li><Link to="/" >Home</Link></li>
-                            {/* <li className="has-droupdown"><Link to="/service" >Service</Link>
-                                <ul className="submenu">
-                                    <li><Link to="/service">Service</Link></li>
-                                    <li><Link to="/service-details">Service Details</Link></li>
-                                </ul>
-                            </li>
-                            <li className="has-droupdown"><Link to="/service" >Blog</Link>
-                                <ul className="submenu">
-                                    <li><Link to="/blog">Blog List</Link></li>
-                                    <li><Link to="/blog-details">Blog Details</Link></li>
-                                </ul>
-                            </li>
-                            <li className="has-droupdown"><Link to="#pages" >Portfolio</Link>
-                                <ul className="submenu">
-                                    <li><Link to="/portfolio">Portfolio</Link></li>
-                                    <li><Link to="/portfolio-details">Portfolio Details</Link></li>
-                                    <li><Link to="/404">404</Link></li>
-                                </ul>
-                            </li> */}
-                            {/* <li className="has-droupdown"><Link to="#" >Blocks</Link>
-                                <ul className="submenu">
-                                    <li><Link to="/portfolio">Portfolio</Link></li>
-                                    <li><Link to="/team">Team</Link></li>
-                                    <li><Link to="/service">Service</Link></li>
-                                    <li><Link to="/video-popup">Video Popup</Link></li>
-                                    <li><Link to="/progressbar">Progressbar</Link></li>
-                                    <li><Link to="/gallery">Gallery</Link></li>
-                                    <li><Link to="/counters">Counters</Link></li>
-                                    <li><Link to="/blog">Blog List</Link></li>
-                                    <li><Link to="/clint-logo">Clint Logo</Link></li>
-                                    <li><Link to="/contact-form">Contact Form</Link></li>
-                                    <li><Link to="/google-map">Google Map</Link></li>
-                                    <li><Link to="/columns">Columns</Link></li>
-                                    <li><Link to="/pricing-table">Pricing Table</Link></li>
-                                </ul>
-                            </li> */}
                             <li><Link to="/service" >Service</Link></li>
                             <li><Link to="/blog" >Blog</Link></li>
-                            {/* <li><Link to="/portfolio" >Portfolio</Link></li> */}
-                            {/* <li><Link to="/products" >Products</Link></li> */}
                             <li><Link to="/extensions" >Extensions</Link></li>
                             <li><Link to="/contact" >Contact</Link></li>
                             <li><Link to="/about" >About</Link></li>
                             {(location.pathname !== '/login' && !token) && <li><Link to="/login">Login</Link></li>}
-                            {/* {(location.pathname !== '/signup' && !token) && <li><Link to="/signup">Sign Up</Link></li>} */}
                             {token && <li onClick={logout} > <Link >Logout</Link></li>}
-                            {/* <li><Link to="/signup" >Sign Up</Link></li> */}
-                            {/* <li><Link to="/signup" >Sign Up</Link></li> */}
                         </ul>
                     </nav>
-                    {/* <div className="header-btn">
-                        <a className="rn-btn" href="/signup">
-                            <span>Sign Up</span>
-                        </a>
-                    </div> */}
+
                     {/* Start Humberger Menu  */}
                     <div className="humberger-menu d-block d-lg-none pl--20">
                         <span onClick={menuTrigger} className="menutrigger text-white"><FiMenu /></span>
