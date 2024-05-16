935e44bab3ff24f6bd3d5f88348f630dc659aee7
sreyasilp <er.sreyas.tp@gmail.com>
2024-05-16 15:12:04 +0530

    change login

--- client/src/elements/login/LoginForm.jsx
+++ client/src/elements/login/LoginForm.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useHistory, Link } from "react-router-dom";
 import axios from "axios";
 import { FcGoogle } from "react-icons/fc";
-import { GoogleOAuthProvider, useGoogleLogin, GoogleLogin } from "@react-oauth/google";
-import { signIn } from "../../api/index.js"; // Import the signIn function from your API file
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import { signIn } from "../../api/index.js";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -11,63 +11,51 @@ function LoginForm() {
   const history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [user, setUser] = useState(null); // New state to store Google user data
+  const [user, setUser] = useState(null);
 
-  // Handle Google login success
   const googleSuccess = async (res) => {
-    const profileObj = res?.profileObj;
-    const tokenId = res?.tokenId;
+    const profileObj = res.profileObj;
+    const tokenId = res.tokenId;
 
     try {
-      // Store the token in local storage
       localStorage.setItem('token', tokenId);
       showToastMessage("Logged in successfully!", false);
-      // Redirect to the '/' page after successful login
       history.push("/");
-
-      // Log the Google login response
       console.log("Google login response:", res);
     } catch (error) {
       console.log(error);
     }
   };
 
-  // Handle Google login failure
   const googleError = () => {
     console.log('Google Sign In was unsuccessful. Try again later');
   };
 
-  // Function to show toast message
   const showToastMessage = (message, error) => {
     if (error) {
       toast.error(message, {
         position: toast.POSITION.TOP_RIGHT,
-        autoClose: 5000, // Will close after 5 seconds
+        autoClose: 5000,
       });
     } else {
       toast.success(message, {
         position: toast.POSITION.TOP_RIGHT,
-        autoClose: 5000, // Will close after 5 seconds
+        autoClose: 5000,
       });
     }
   };
 
-  // Function to handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      // Call the signIn function from API file with form data
       const response = await signIn({
         email: email,
         password: password,
       });
-      // Store the token in local storage
       localStorage.setItem('token', response.data.token);
       showToastMessage("Logged in successfully!", false);
-      // Redirect to the '/' page after successful login
       history.push("/");
     } catch (error) {
-      // Handle error
       showToastMessage(error.response.data.message, true);
     }
   };
@@ -106,7 +94,7 @@ function LoginForm() {
                     />
                   </label>
                   <p style={{ color: 'black', marginTop: '0px' }}>
-                    Does't have an Account ? <Link to="/signup" style={{ color: "#f9004d", fontWeight: 'bold' }}>Sign Up</Link>
+                    Doesn't have an Account? <Link to="/signup" style={{ color: "#f9004d", fontWeight: 'bold' }}>Sign Up</Link>
                   </p>
                   <button
                     className="rn-button-style--2 btn-solid"
@@ -118,10 +106,7 @@ function LoginForm() {
                     Submit
                   </button>
 
-
                   <GoogleLogin
-                    clientId="168821784143-d0q7nugflesop4nh6rbdp3f95sr6o9c8.apps.googleusercontent.com"
-                    buttonText="Login with Google"
                     onSuccess={googleSuccess}
                     onFailure={googleError}
                     cookiePolicy={'single_host_origin'}
