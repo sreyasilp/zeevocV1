ec8206adcf59bb5fd022587ff6563656944a06d7
sreyasilp <er.sreyas.tp@gmail.com>
2024-05-19 16:42:00 +0530

    magento 2

--- client/src/elements/blog/BlogDetails.jsx
+++ client/src/elements/blog/BlogDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { FiClock, FiUser, FiMessageCircle, FiHeart } from "react-icons/fi";
+import { FiClock, FiUser, FiMessageCircle, FiHeart, FiCopy } from "react-icons/fi";
 import { Link, useParams } from 'react-router-dom';
 import ScrollToTop from 'react-scroll-up';
 import { FiChevronUp } from "react-icons/fi";
@@ -24,16 +24,22 @@ const BlogDetails = () => {
                 const response = await getBlogById(blogId);
                 setBlogData(response.data);
             } catch (error) {
-                console.error("Error fetching extension data:", error);
+                console.error("Error fetching blog data:", error);
             }
         };
 
         fetchData();
     }, [blogId]);
 
+    const copyToClipboard = (text) => {
+        navigator.clipboard.writeText(text).then(() => {
+            // alert('Code copied to clipboard!');
+        });
+    };
+
     return (
         <React.Fragment>
-            <PageHelmet pageTitle={blogData.title} /> 
+            <PageHelmet pageTitle={blogData.title} />
             <Header headertransparent="header--transparent" colorblack="color--black" logoname="logo.png" />
 
             {/* Start Breadcrump Area */}
@@ -42,7 +48,7 @@ const BlogDetails = () => {
                     <div className="row">
                         <div className="col-lg-12">
                             <div className="blog-single-page-title text-center pt--100">
-                            <h2 className="title theme-gradient">{blogData.title}</h2> 
+                                <h2 className="title theme-gradient">{blogData.title}</h2>
                                 <ul className="blog-meta d-flex justify-content-center align-items-center">
                                     <li><FiClock />May 18, 2020</li>
                                     <li><FiUser />NipaBali</li>
@@ -63,48 +69,90 @@ const BlogDetails = () => {
                         <div className="col-lg-12">
                             <div className="inner-wrapper">
                                 <div className="inner">
-                                    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum. You need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend toitrrepeat predefined chunks. </p>
+                                <h3 className="title">Steps to Install Magento on ubuntu</h3>
+
+                                    <p>Magento is a powerful and flexible open-source e-commerce platform that provides a wide range of features and customization options. Installing Magento correctly ensures a stable and secure online store. Follow the steps below to install Magento on your server.</p>
                                     <div className="thumbnail">
                                         <img src="/assets/images/blog/bl-big-01.jpg" alt="Blog Images" />
                                     </div>
-                                    <p className="mt--40">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum. You need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend toitrrepeat predefined chunks. Necessary, making this the first true generator on the Internet. It re are many variations of passages of Lo rem Ipsum available, but the majority have suffered alteration in some form, by injectedeed eedhumour, or randomised words which don't look even slightly believable.</p>
-                                    <p>Necessary, making this the first true generator on the Internet. It re are many variations of passages of Lo
-                                    rem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
-                                    <blockquote className="rn-blog-quote">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.</blockquote>
-                                    <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum. You need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend toitrrepeat predefined chunks. Necessary, making this the first true generator on the Internet. It re are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injectedeed eedhumour, or randomised words which don't look even slightly believable.</p>
-                                    <div className="blog-single-list-wrapper d-flex flex-wrap">
-                                        <div className="thumbnail">
-                                            <img className="w-" src="/assets/images/blog/blog-single-01.png" alt="BLog Images" />
-                                            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do</span>
-                                        </div>
-                                        <div className="content">
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo .</p>
-                                            <h4 className="title">Ordered & Unordered Lists.</h4>
-                                            <ul className="list-style">
-                                                <li>Yet this above sewed flirted opened ouch</li>
-                                                <li>Goldfinch realistic sporadic ingenuous</li>
-                                                <li>Abominable this abidin far successfully then like piquan</li>
-                                                <li>Risus commodo viverra</li>
-                                                <li>Lorem ipsum dolor sit amet, consectetur adipiscing</li>
-                                            </ul>
-                                            <h4 className="title">Ordered & Unordered Lists.</h4>
-                                            <ul className="list-style">
-                                                <li>Yet this above sewed flirted opened ouch</li>
-                                                <li>Goldfinch realistic sporadic ingenuous</li>
-                                                <li>Abominable this abidin far successfully then like piquan</li>
-                                                <li>Risus commodo viverra</li>
-                                            </ul>
-                                        </div>
+                                    <p className="mt--40">Before you begin, ensure your server meets the necessary system requirements for running Magento, such as the correct PHP version, required PHP extensions, and appropriate server settings.</p>
+                                    <blockquote className="rn-blog-quote">"Setting up Magento correctly from the start is crucial for the performance and security of your online store."</blockquote>
+                                    <p>Magento requires a database to store all its data. You can create a new database for Magento using your hosting control panel or via the command line interface.</p>
+
+                                    {/* Magento Installation Steps */}
+                                    <div className="installation-steps">
+                                        <h3 className="title">Steps to Install Magento</h3>
+                                        <ol>
+                                            <li>
+                                                <h4 className="step-title">Step 1: System Requirements</h4>
+                                                <p>Ensure your server meets the system requirements for Magento. This includes having the correct PHP version, necessary extensions, and appropriate server settings.</p>
+                                            </li>
+                                            <li>
+                                                <h4 className="step-title">Step 2: Download Magento</h4>
+                                                <p>Download the Magento software from the official website or via Composer. Use the following command to download Magento via Composer:</p>
+                                                <div className="code-snippet">
+                                                    <pre style={{ background: 'black', color: 'white', padding: '10px', borderRadius: '5px' }}>
+                                                        <code>composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=2.4.3</code>
+                                                    </pre>
+                                                    <button className="copy-button" onClick={() => copyToClipboard('composer create-project --repository-url=https://repo.magento.com/ magento/project-community-edition=2.4.3')}>
+                                                        <FiCopy /> Copy
+                                                    </button>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <h4 className="step-title">Step 3: Set Up Database</h4>
+                                                <p>Create a new database for Magento to use. You can do this via your hosting control panel or by using the command line:</p>
+                                                <div className="code-snippet">
+                                                    <pre style={{ background: 'black', color: 'white', padding: '10px', borderRadius: '5px' }}>
+                                                        <code>CREATE DATABASE magento;</code>
+                                                    </pre>
+                                                    <button className="copy-button" onClick={() => copyToClipboard('CREATE DATABASE magento;')}>
+                                                        <FiCopy /> Copy
+                                                    </button>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <h4 className="step-title">Step 4: Configure Magento</h4>
+                                                <p>Run the Magento setup wizard or use the command line to configure Magento. Ensure you provide the necessary database and admin user information. Example command:</p>
+                                                <div className="code-snippet">
+                                                    <pre style={{ background: 'black', color: 'white', padding: '10px', borderRadius: '5px' }}>
+                                                        <code>bin/magento setup:install --base-url=http://your-domain.com/ --db-host=localhost --db-name=magento --db-user=root --db-password=yourpassword --admin-firstname=Admin --admin-lastname=User --admin-email=admin@your-domain.com --admin-user=admin --admin-password=admin123</code>
+                                                    </pre>
+                                                    <button className="copy-button" onClick={() => copyToClipboard('bin/magento setup:install --base-url=http://your-domain.com/ --db-host=localhost --db-name=magento --db-user=root --db-password=yourpassword --admin-firstname=Admin --admin-lastname=User --admin-email=admin@your-domain.com --admin-user=admin --admin-password=admin123')}>
+                                                        <FiCopy /> Copy
+                                                    </button>
+                                                </div>
+                                            </li>
+                                            <li>
+                                                <h4 className="step-title">Step 5: Finalize Installation</h4>
+                                                <p>Set the correct file permissions and deploy static content. Example commands:</p>
+                                                <div className="code-snippet">
+                                                    <pre style={{ background: 'black', color: 'white', padding: '10px', borderRadius: '5px' }}>
+                                                        <code>
+                                                            {`find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {} + && \\
+find var generated vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} + && \\
+chown -R :www-data . && chmod u+x bin/magento\nbin/magento setup:static-content:deploy -f`}
+                                                        </code>
+                                                    </pre>
+                                                    <button className="copy-button" onClick={() => copyToClipboard(`find var generated vendor pub/static pub/media app/etc -type f -exec chmod g+w {} + && \nfind var generated vendor pub/static pub/media app/etc -type d -exec chmod g+ws {} + && \nchown -R :www-data . && chmod u+x bin/magento\nbin/magento setup:static-content:deploy -f`)}>
+                                                        <FiCopy /> Copy
+                                                    </button>
+                                                </div>
+                                            </li>
+                                        </ol>
+                                        {/* <div className="thumbnail">
+                                            <img src="/assets/images/blog/bl-big-01.jpg" alt="Magento Installation" />
+                                        </div> */}
                                     </div>
 
-                                    <p className="mt--25 mt_sm--5">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum. You need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend toitrrepeat predefined chunks. Necessary, making this the first true generator on the Internet. It re are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injectedeed eedhumour, or randomised words which don't look even slightly believable.</p>
+                                    <p className="mt--25 mt_sm--5">Once the installation is complete, you can access the Magento admin panel by navigating to your store's base URL followed by /admin. From there, you can begin setting up your store, adding products, and configuring settings to meet your business needs.</p>
                                     <div className="video-wrapper position-relative mb--40">
                                         <div className="thumbnail">
                                             <img src="/assets/images/blog/bl-big-01.jpg" alt="Blog Images" />
                                         </div>
                                         <button className="video-popup position-top-center" onClick={openModal}><span className="play-icon"></span></button>
                                     </div>
-                                    <p className="mb--0">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum. You need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend toitrrepeat predefined chunks. Necessary, making this the first true generator on the Internet. It re are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injectedeed eedhumour, or randomised words which don't look even slightly believable.</p>
+                                    <p className="mb--0">Magento's flexibility and scalability make it a popular choice for businesses of all sizes. With a large community of developers and a wide range of extensions available, Magento can be tailored to suit the specific needs of your online store.</p>
                                 </div>
                             </div>
                         </div>
@@ -113,7 +161,7 @@ const BlogDetails = () => {
             </div>
             {/* End Blog Details */}
 
-            {/* Start BLog Comment Form  */}
+            {/* Start Blog Comment Form  */}
             <div className="blog-comment-form pb--120 bg_color--1">
                 <div className="container">
                     <div className="row">
@@ -137,7 +185,6 @@ const BlogDetails = () => {
                                             <div className="rnform-group">
                                                 <textarea type="text" placeholder="Comment"></textarea>
                                             </div>
-
                                         </div>
                                         <div className="col-lg-12">
                                             <div className="blog-btn">
@@ -151,7 +198,7 @@ const BlogDetails = () => {
                     </div>
                 </div>
             </div>
-            {/* End BLog Comment Form  */}
+            {/* End Blog Comment Form  */}
 
             {/* Start Back To Top */}
             <div className="backto-top">
@@ -162,7 +209,6 @@ const BlogDetails = () => {
             {/* End Back To Top */}
 
             <Footer />
-
         </React.Fragment>
     )
 }
